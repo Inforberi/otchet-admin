24f74a8d5218e529b6530beb9ab22a17438ae455
Алексей <tatarchuk.a.s.1997@gmail.com>
2026-01-16 12:48:33 +0300

    feat: добавлена миграция для размеров шрифтов в отчетах

---
+++ prisma/migrations/20260116084949_add_font_sizes_to_reports/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "reports" ADD COLUMN     "caption_font_size" TEXT DEFAULT '16',
+ADD COLUMN     "description_font_size" TEXT DEFAULT '20',
+ADD COLUMN     "title_font_size" TEXT DEFAULT '40';
