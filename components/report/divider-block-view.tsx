994fbf5c1710f690829b015f7b9c8fe1898500f6
Aleksey Tatarchuk <tatarchuk.a.s.1997@gmail.com>
2026-01-20 23:23:22 +0300

    fix: симметричные отступы и белый цвет для divider блока

--- components/report/divider-block-view.tsx
+++ components/report/divider-block-view.tsx
@@ -1,7 +1,7 @@
 export function DividerBlockView() {
   return (
-    <div className="my-8">
-      <hr className="border-t border-gray-300" />
+    <div className="my-20">
+      <hr className="border-t border-white" />
     </div>
   )
 }
